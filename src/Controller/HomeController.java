422a8c9c5f1c4a9fee2ce8fd8f7d7c4f88f02576
Jai Aggarwal <jai.aggarwal@mail.utoronto.ca>
2018-01-09 00:46:24 -0500

    added audio to home page

--- src/Controller/HomeController.java
+++ src/Controller/HomeController.java
@@ -2,10 +2,14 @@ package Controller;
 
 import View.MonsterChoiceGUI;
 import javafx.scene.Scene;
+import javafx.scene.media.AudioClip;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 public class HomeController {
     Stage stage;
+    AudioClip clip;
 
     public HomeController(Stage mainstage){
         stage = mainstage;
@@ -13,6 +17,19 @@ public class HomeController {
     public void next() {
         Scene scene = new Scene(new MonsterChoiceGUI(stage), 500, 275);
         scene.getStylesheets().add("Styles/MonsterChoiceStyle.css");
+        clip.stop();    
         stage.setScene(scene);
+    }
+
+    public void playClank(){
+        URL sound = getClass().getResource("/audio/clank.wav");
+        clip = new AudioClip(sound.toString());
+        clip.play();
+    }
+
+    public void playMeadow(){
+        URL sound = getClass().getResource("/audio/meadow.wav");
+        clip = new AudioClip(sound.toString());
+        clip.play();
     }
 }
--- src/View/HomeGUI.java
+++ src/View/HomeGUI.java
@@ -3,15 +3,19 @@ package View;
 import Controller.HomeController;
 import javafx.animation.FadeTransition;
 import javafx.animation.ScaleTransition;
+import javafx.animation.SequentialTransition;
 import javafx.animation.TranslateTransition;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.media.AudioClip;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+
+import java.net.URL;
 
 
 public class HomeGUI extends Pane {
@@ -19,6 +23,11 @@ public class HomeGUI extends Pane {
     public HomeGUI(Stage mainStage) {
         Stage stage = mainStage;
         HomeController hc = new HomeController(stage);
+
+        final URL resource = getClass().getResource("/audio/clank.wav");
+//        System.out.println(resource);
+        final AudioClip clip = new AudioClip(resource.toString());
+
 
         ImageView top = new ImageView(new Image("images/meadow_top.jpg", 500, 140, false, false));
         ImageView bottom = new ImageView(new Image("images/meadow_bottom.jpg", 500, 143, false, false));
@@ -34,12 +43,18 @@ public class HomeGUI extends Pane {
         TranslateTransition tt1 = new TranslateTransition(Duration.seconds(1), top);
         tt1.setFromY(-100);
         tt1.setToY(0);
-        tt1.play();
+        tt1.setOnFinished(e -> hc.playClank());
+//        tt1.play();
 
         TranslateTransition tt2 = new TranslateTransition(Duration.seconds(1), bottom);
         tt2.setFromY(300);
         tt2.setToY(140);
-        tt2.play();
+        tt2.setOnFinished(e -> hc.playClank());
+//        tt2.play();
+
+        SequentialTransition sequentialTransition = new SequentialTransition(tt1, tt2);
+        sequentialTransition.play();
+        sequentialTransition.setOnFinished(e -> hc.playMeadow());
 
         TranslateTransition ttmelee = new TranslateTransition(Duration.seconds(3), melee);
         ttmelee.setToX(300);
@@ -62,6 +77,8 @@ public class HomeGUI extends Pane {
         fadeTransition.setToValue(1);
         fadeTransition.setDelay(Duration.seconds(3));
         fadeTransition.play();
+
+
 
         this.getChildren().addAll(top, bottom, melee, meadows, next);
 
